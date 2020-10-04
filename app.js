8b9201d30b2d68af5ddaff578c9667109e3d85c0
rbdkqk <rbdkqk@gmail.com>
2020-10-04 15:25:10 +0900

    edit : app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const session = require("express-session");
+const session = require("express-session"); // express 에서 세션을 다룰 때 사용되는 미들웨어
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan"); // 로그 관리를 위한 미들웨어. (토큰도 가능한듯?)
+const bodyParser = require("body-parser"); // 요청에서 JSON을 파싱할때 사용되는 미들웨어
 
 /* 이상 모듈 목록 */
 
@@ -17,7 +19,9 @@ app.use(
   })
 );
 
+app.use(morgan("dev")); // format을 설정할 수 있다. 참고 : https://m.blog.naver.com/PostView.nhn?blogId=p952973&logNo=220995349123&proxyReferer=https:%2F%2Fwww.google.com%2F
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
